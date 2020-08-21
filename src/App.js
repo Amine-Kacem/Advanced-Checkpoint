6ae2dc1aa71f33b9e69387cc88b613aa1b519b4a
unknown <kacem.amine1112@gmail.com>
2020-08-21 01:53:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import * as ReactBootStrap from "react-bootstrap";
 
-function App() {
+const App = () => {
+  const Products = [
+    { name: "Iphone 11", category: "Electronic", price: 1100 },
+    { name: "T-shirt Juventus", category: "Clothes", price: 110 },
+    { name: "Dell PC Gamer", category: "Electronic", price: 2500 },
+  ];
+
+  const renderProduct = (Product, index) => {
+    return (
+      <tr key={index}>
+        <td>{Product.name}</td>
+        <td>{Product.category}</td>
+        <td>{Product.price}</td>
+      </tr>
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Table Products</h1>
+      <ReactBootStrap.Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Category</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{Products.map(renderProduct)}</tbody>
+      </ReactBootStrap.Table>
     </div>
   );
-}
+};
 
 export default App;
